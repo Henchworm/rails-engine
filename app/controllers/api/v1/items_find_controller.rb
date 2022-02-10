ec8bac15c8353c2ba52c0bf088c3aa3a1b541937
Chris Hewitt <agop5134@gmail.com>
2022-02-09 21:54:55 -0700

    start paramss missing/nil filter on all controllers. needs some specs

--- app/controllers/api/v1/items_find_controller.rb
+++ app/controllers/api/v1/items_find_controller.rb
@@ -8,10 +8,15 @@ class Api::V1::ItemsFindController < ApplicationController
     else
     items = Item.where("name ILIKE ?", "%#{params[:name]}%")
     render json: ItemSerializer.new(items)
+    end
   end
 
   def show
-
+    if !params[:name]
+      render json: { errors: { details: 'No params.' } }, status: 400
+    elsif params[:name] == ''
+      render json: { errors: { details: 'Empty params.' } }, status: 400
+    else
       item = Item.where("name ILIKE ?", "%#{params[:name]}%")
       if item.first != nil
         json_response(item.first)
--- app/controllers/api/v1/merchants_find_controller.rb
+++ app/controllers/api/v1/merchants_find_controller.rb
@@ -1,8 +1,14 @@
 class Api::V1::MerchantsFindController < ApplicationController
 
   def index
-    merchant_matches = Merchant.where("name ILIKE ?", "%#{params[:name]}%")
-    render json: MerchantSerializer.new(merchant_matches)
+    if !params[:name]
+      render json: { errors: { details: 'No params.' } }, status: 400
+    elsif params[:name] == ''
+      render json: { errors: { details: 'Empty params.' } }, status: 400
+    else
+      merchant_matches = Merchant.where("name ILIKE ?", "%#{params[:name]}%")
+      render json: MerchantSerializer.new(merchant_matches)
+    end
   end
 
   def show
