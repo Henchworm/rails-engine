2b7155dcb45c5fe81768bdfaad8644e662f37403
Chris Hewitt <agop5134@gmail.com>
2022-02-10 17:38:02 -0700

    passing postman harnass 100%, now need to update rspec for coverage

--- app/controllers/api/v1/items_find_controller.rb
+++ app/controllers/api/v1/items_find_controller.rb
@@ -1,28 +1,52 @@
 class Api::V1::ItemsFindController < ApplicationController
+  before_action :find_filter
 
   def index
-    if !params[:name]
-      render json: { errors: { details: 'No params.' } }, status: 400
-    elsif params[:name] == ''
-      render json: { errors: { details: 'Empty params.' } }, status: 400
-    else
     items = Item.where("name ILIKE ?", "%#{params[:name]}%")
     render json: ItemSerializer.new(items)
-    end
   end
 
   def show
-    if !params[:name]
-      render json: { errors: { details: 'No params.' } }, status: 400
-    elsif params[:name] == ''
-      render json: { errors: { details: 'Empty params.' } }, status: 400
-    else
-      item = Item.where("name ILIKE ?", "%#{params[:name]}%")
-      if item.first != nil
+    item = Item.where("name ILIKE ?", "%#{params[:name]}%")
+    if item.first != nil
         json_response(item.first)
-      else
-        render json: {data: {object: ItemSerializer.new(item) }}
-      end
+    else
+      render json: {data: {object: ItemSerializer.new(item) }}
+    end
+  end
+
+  def find_by_min_price
+    item = Item.where("items.unit_price >= ?", params[:min_price].to_f).order(:name)
+    if item.first.class != Item
+      render json: {data: { item: []}}, status: 400
+    else
+    render json: ItemSerializer.new(item.first)
+    end
+  end
+
+  def find_by_max_price
+    item = Item.where("items.unit_price <= ?", params[:max_price].to_f).order(:name)
+    if item.first.class != Item
+      render json: {data: { item: []}}, status: 400
+    else
+    render json: ItemSerializer.new(item.first)
+    end
+  end
+
+  private
+  def find_filter
+    if params[:name] && params[:min_price].present?
+      name_price_filter
+    elsif params[:name] && params[:max_price].present?
+      name_price_filter
+    elsif params[:min_price].present? && params[:max_price].present?
+      price_comparison_filter
+    elsif params[:min_price].present?
+      min_price_filter
+    elsif params[:max_price].present?
+      max_price_filter
+    elsif params.has_key?(:name) || params[:name] == nil
+      name_filter
     end
   end
 end
--- app/controllers/api/v1/merchants_find_controller.rb
+++ app/controllers/api/v1/merchants_find_controller.rb
@@ -1,30 +1,18 @@
 class Api::V1::MerchantsFindController < ApplicationController
+  before_action :name_filter
 
   def index
-    if !params[:name]
-      render json: { errors: { details: 'No params.' } }, status: 400
-    elsif params[:name] == ''
-      render json: { errors: { details: 'Empty params.' } }, status: 400
-    else
-      merchant_matches = Merchant.where("name ILIKE ?", "%#{params[:name]}%")
-      render json: MerchantSerializer.new(merchant_matches)
-    end
+    merchant_matches = Merchant.where("name ILIKE ?", "%#{params[:name]}%")
+    render json: MerchantSerializer.new(merchant_matches)
   end
 
   def show
-    if !params[:name]
-      render json: { errors: { details: 'No params.' } }, status: 400
-    elsif params[:name] == ''
-      render json: { errors: { details: 'Empty params.' } }, status: 400
-    else
-      merchant_matches = Merchant.where("name ILIKE ?", "%#{params[:name]}%")
-      merchant = merchant_matches.return_one
+    merchant_matches = Merchant.where("name ILIKE ?", "%#{params[:name]}%")
+    merchant = merchant_matches.return_one
     if merchant != nil
       json_response(merchant)
     else
       render json: { data: {:merchant => [] }}
     end
-    end
   end
-
 end
