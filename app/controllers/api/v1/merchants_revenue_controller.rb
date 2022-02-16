51e7bcc6bac96f24f29a660e7745c8076d241778
Chris Hewitt <agop5134@gmail.com>
2022-02-15 22:34:33 -0700

    pass postman merchant_revenue

--- app/controllers/api/v1/merchants_revenue_controller.rb
+++ app/controllers/api/v1/merchants_revenue_controller.rb
@@ -2,8 +2,6 @@ class Api::V1::MerchantsRevenueController < ApplicationController
 
   def show
     merchant = Merchant.find(params[:id])
-    Merchant.total_revenue(merchant.id)
-    json_response(Merchant.find(params[:id]))
+    render json: {data: {type: "merchant_revenue", id: merchant.id.to_s, attributes: {revenue: Merchant.total_revenue(merchant.id)}}}
   end
-
 end
